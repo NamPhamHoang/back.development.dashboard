ecc99b8dd8dc2b15e98f43d0640479e16460858d
NamPhamHoang <phamhoangnam55@gmail.com>
2020-12-07 18:10:34 +0700

    attachment and fetch full_project api

--- school/Back.Development.Dashboard/src/boot/services/api.ts
+++ school/Back.Development.Dashboard/src/boot/services/api.ts
@@ -6,7 +6,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import { listen } from "~@/boot/services/socket";
 import {
-    chatThread
+  chatThread
 } from "~@/routes/api"
 import qs from "qs";
 import moment from "moment";
@@ -17,47 +17,47 @@ const app = express();
 const tempUpload = multer({
   storage: multer.memoryStorage()
 });
-if(process.env.IS_LISTEN_WS === "1") {
-    listen()
+if (process.env.IS_LISTEN_WS === "1") {
+  listen()
 }
 app.use(
-    //@TODO: add to env
-    cors({
-        origin: "*",
-    })
+  //@TODO: add to env
+  cors({
+    origin: "*",
+  })
 );
 
 app.use(
-    "/hasura",
-    createProxyMiddleware({
-      target: `${process.env.HASURA_ENDPOINT}`,
-      logLevel: "error",
-      pathRewrite: (path) => {
-        const pathSlashArr = path.split("/").filter((str) => str.length > 0);
-        if (pathSlashArr.length <= 1) return "/";
-        pathSlashArr.shift();
-        return "/" + pathSlashArr.join("/");
-      },
-  
-      ws: true,
-      followRedirects: true,
-    })
-  );
-  
+  "/hasura",
+  createProxyMiddleware({
+    target: `${process.env.HASURA_ENDPOINT}`,
+    logLevel: "error",
+    pathRewrite: (path) => {
+      const pathSlashArr = path.split("/").filter((str) => str.length > 0);
+      if (pathSlashArr.length <= 1) return "/";
+      pathSlashArr.shift();
+      return "/" + pathSlashArr.join("/");
+    },
+
+    ws: true,
+    followRedirects: true,
+  })
+);
+
 app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
 
 
 app.use((err, req, res, next) => {
-    console.log(err);
-    return res.status(err.status || 500).json({
-        isError: true,
-        payload: err.toString(),
-    });
+  console.log(err);
+  return res.status(err.status || 500).json({
+    isError: true,
+    payload: err.toString(),
+  });
 });
 
 app.post(
@@ -105,9 +105,9 @@ app.post(
     }
   }
 );
-app.get("/chat_thread/:id",chatThread);
-app.post("/message/:thread_id", async (req,res) => {
-    const threadID = req.params.thread_id;
+app.get("/chat_thread/:id", chatThread);
+app.post("/message/:thread_id", async (req, res) => {
+  const threadID = req.params.thread_id;
   const message = req.body.message;
   console.log(message, threadID);
   try {
@@ -139,9 +139,21 @@ app.post("/message/:thread_id", async (req,res) => {
 app.get("/full_project/:project_id", async (req, res) => {
   try {
     const data = await fetchFullProjectInformation(req.params.project_id);
+    const parseData = {
+      id: data.id,
+      title: data.title,
+      text: data.description,
+      minbudget: data.budget.minimum,
+      maxbudget: data.budget.maximum,
+      linkUrl: data.seo_url,
+      appended_descr: data.preview_description,
+      status: data.status,
+      created_at: data.submitdate,
+      currency: data.currency.code
+    }
     return res.json({
-      isError: false, 
-      message: data
+      isError: false,
+      message: parseData
     })
   } catch (err) {
     res.status(500).json({
@@ -187,7 +199,7 @@ app.get("/attachment/", async (req, res) => {
 });
 
 
-app.post("/message-attachment/:thread_id", tempUpload.single("file"), async (req,res) => {
+app.post("/message-attachment/:thread_id", tempUpload.single("file"), async (req, res) => {
   const threadID = req.params.thread_id;
   try {
     request.post(`https://www.freelancer.com/api/messages/0.1/threads/${threadID}/messages/?compact=true&new_errors=true`, {
@@ -225,9 +237,9 @@ app.post("/message-attachment/:thread_id", tempUpload.single("file"), async (req
   }
 });
 (async () => {
-    app.listen(process.env.PORT, () => {
-        log.info(`> web application is runnning on PORT: ${process.env.PORT}`, {
-            port: process.env.PORT,
-        });
+  app.listen(process.env.PORT, () => {
+    log.info(`> web application is runnning on PORT: ${process.env.PORT}`, {
+      port: process.env.PORT,
     });
+  });
 })();
