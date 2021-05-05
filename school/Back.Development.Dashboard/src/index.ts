cac2f976fcce440cdaac66644585a5391d6b7e74
NamPhamHoang <phamhoangnam55@gmail.com>
2021-05-05 16:22:05 +0700

    remove unnessary file

--- school/Back.Development.Dashboard/src/index.ts
+++
@@ -1,7 +0,0 @@
-if (process.env.NODE_ENV === "production") {
-    require("module-alias/register");
-    require("../dotenv.config");
-  }
-  import boot from "~@/boot/index";
-  boot();
-  
--- school/Back.Development.Dashboard/src/utils/functions/filters/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as isExist } from "./exist.filter";
-export { default as isSkill } from "./skills.filter"; 
-export { default as isGoodBudget } from "./budget.filter";
